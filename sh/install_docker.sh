8d78a5082a3e2b567fcfc4ae5796cc0183c42d4d
王贺 <wanghe@fit2cloud.com>
2025-04-18 15:09:48 +0800

    Update install_docker.sh

--- sh/install_docker.sh
+++ sh/install_docker.sh
@@ -1,1 +1,11 @@
-bash <(curl -sSL https://linuxmirrors.cn/docker.sh)
+#!/bin/bash
+
+country=$(curl -s ipinfo.io/country)
+echo "Current server location: $country"
+
+if [[ "$country" == "CN" ]]; then
+  bash <(curl -sSL https://linuxmirrors.cn/docker.sh)
+else
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+fi
